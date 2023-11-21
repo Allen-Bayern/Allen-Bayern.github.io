b58d4c755f0df840b42bc250378e52d1eaec48ce
yueb <zhaozhengyang_byn@pku.edu.cn>
2023-11-21 23:08:53 +0800

    feat: 重置

--- assets/js/page--node-modules--gridsome--app--pages--404-vue.c03f8cf5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{XtlX:function(n,t,u){"use strict";u.r(t);u=u("KHd+"),u=Object(u.a)({},function(){return(0,this._self._c)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
