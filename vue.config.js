ede64812d41b4144c07b29c909897606a0cfb3e7
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-02-21 23:26:52 +0800

    refactor: reset of the project and add css reset and gh-pages deploy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,34 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
+const { defineConfig } = require('@vue/cli-service');
+
+/** @description Add global style-resource for scss file */
+function addStyleResource(rule, patterns = []) {
+    rule.use('style-resource').loader('style-resources-loader').options({
+        patterns,
+    });
+}
+
+/** @description the configuration of this project */
+const vueConfig = defineConfig({
+    transpileDependencies: true,
+    lintOnSave: 'error',
+    devServer: {
+        port: 7500,
+    },
+    // use webpack chain
+    chainWebpack(config) {
+        ['vue-modules', 'vue', 'normal-modules', 'normal'].forEach(type => {
+            addStyleResource(config.module.rule('scss').oneOf(type));
+        });
+
+        config
+            .plugin('html')
+            .tap(args => {
+                const [basicConf] = args;
+                // Modify Title
+                return [{ ...basicConf, title: '月饼叔叔的小屋' }];
+            })
+            .end();
+    },
 });
+
+module.exports = vueConfig;
