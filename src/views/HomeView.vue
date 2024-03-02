2b4a55601d3c86721cd3094f5cd13ee80cc93764
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-03-02 22:15:25 +0800

    feat: add px to vw

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,9 @@
 <template>
     <div class="home">hello</div>
 </template>
+
+<style lang="scss" scoped>
+.home {
+    font-size: 24vpx;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,9 @@
 const { defineConfig } = require('@vue/cli-service');
 
-/** @description Add global style-resource for scss file */
-function addStyleResource(rule, patterns = []) {
-    rule.use('style-resource').loader('style-resources-loader').options({
-        patterns,
-    });
-}
+/** @description Add global style-resource for css / scss / less ... file */
+const addStyleResource = (rule, patterns = []) => {
+    rule.use('style-resource').loader('style-resources-loader').options({ patterns });
+};
 
 /** @description the configuration of this project */
 const vueConfig = defineConfig({
@@ -13,6 +11,42 @@ const vueConfig = defineConfig({
     lintOnSave: 'error',
     devServer: {
         port: 7500,
+    },
+    // config css
+    css: {
+        loaderOptions: {
+            postcss: {
+                postcssOptions: {
+                    plugins: [
+                        // use px-to-vw
+                        require('postcss-px-to-viewport')({
+                            // 用vpx, 而非占用px
+                            unitToConvert: 'vpx',
+
+                            // 二倍图
+                            viewportWidth: 375,
+                            landscapeWidth: 667,
+
+                            unitPrecision: 5,
+                            propList: ['*'],
+                            viewportUnit: 'vw',
+                            fontViewportUnit: 'vw',
+                            selectorBlackList: [],
+                            minPixelValue: 1,
+
+                            // use media query
+                            mediaQuery: true,
+
+                            replace: true,
+                            exclude: undefined,
+                            include: undefined,
+                            landscape: false,
+                            landscapeUnit: 'vw',
+                        }),
+                    ],
+                },
+            },
+        },
     },
     // use webpack chain
     chainWebpack(config) {
