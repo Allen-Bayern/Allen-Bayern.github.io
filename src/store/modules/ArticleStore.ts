a7b2d86adae976cf5500a1c099c2ee9c3a08b46a
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-03-09 23:08:07 +0800

    fix: 路由问题

--- src/store/modules/ArticleStore.ts
+++ src/store/modules/ArticleStore.ts
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia';
 import { importAllArticles, type ArticleLazyReturn } from '@/articles';
-import { toNotFound } from '@/utils';
 
 // 引入文章集
 const allArticles = importAllArticles<ArticleLazyReturn>();
@@ -17,18 +16,11 @@ const useArticleStore = defineStore('articleStore', {
     },
     actions: {
         async getArticle(articleId: string) {
-            try {
-                if (Object.keys(allArticles).includes(articleId)) {
-                    this.currentArticleId = articleId;
-                    // 懒加载文章
-                    const articleCnt = await allArticles[articleId];
-                    this.currentArticle = articleCnt.default;
-                } else {
-                    toNotFound();
-                }
-            } catch {
-                // 如没有, 导航到404页面
-                toNotFound();
+            if (Object.keys(allArticles).includes(articleId)) {
+                this.currentArticleId = articleId;
+                // 懒加载文章
+                const articleCnt = await allArticles[articleId];
+                this.currentArticle = articleCnt?.default;
             }
         },
     },
